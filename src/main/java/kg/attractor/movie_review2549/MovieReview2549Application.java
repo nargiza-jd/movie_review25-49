6215100bf20f1dc2c3ec4b304ad5b71d9a56075f
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-12 16:58:08 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/movie_review2549/MovieReview2549Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review2549;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReview2549Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReview2549Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/movie_review2549/MovieReview2549ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review2549;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReview2549ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
